0d2636b0f16835ffc38b5588d01d32755ac36293
3615 <internet-1@serandour.org>
2021-03-28 21:56:15 +0200

    Create clavier.ino

---
+++ clavier.ino
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////////////////////////
+
+byte getKeyRec() {
+  boolean etatBoutonRec = digitalRead(PIN_BOUTON_REC);
+  if((etatBoutonRec != memoireBoutonRec) && (etatBoutonRec == HIGH)) {
+    valBoutonRec++;
+    valBoutonRec = valBoutonRec % 3; // 3 valeurs possibles : 0, 1, 2
+  }
+  // On enregistre l'état du bouton pour le tour suivant
+  memoireBoutonRec = etatBoutonRec;
+  return valBoutonRec;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+byte getKeyPlay() {
+  boolean etatBoutonPlay = digitalRead(PIN_BOUTON_PLAY);
+  if((etatBoutonPlay != memoireBoutonPlay) && (etatBoutonPlay == HIGH)) {
+    valBoutonPlay++;
+    valBoutonPlay = valBoutonPlay % 2; // 2 valeurs possibles : 0, 1
+  }
+  // On enregistre l'état du bouton pour le tour suivant
+  memoireBoutonPlay = etatBoutonPlay;
+  return valBoutonPlay;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
