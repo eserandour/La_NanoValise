4775044aa463d740caeb131abe39a1be27d48be3
3615 <internet-1@serandour.org>
2021-03-29 22:26:12 +0200

    Update clavier.ino

--- clavier.ino
+++ clavier.ino
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 byte getKeyRec() {
-  boolean etatBoutonRec = digitalRead(PIN_BOUTON_REC);
+  boolean etatBoutonRec = digitalRead(BOUTON_REC);
   if((etatBoutonRec != memoireBoutonRec) && (etatBoutonRec == HIGH)) {
     valBoutonRec++;
     valBoutonRec = valBoutonRec % 3; // 3 valeurs possibles : 0, 1, 2
@@ -14,7 +14,7 @@ byte getKeyRec() {
 //////////////////////////////////////////////////////////////////////////////////////
 
 byte getKeyPlay() {
-  boolean etatBoutonPlay = digitalRead(PIN_BOUTON_PLAY);
+  boolean etatBoutonPlay = digitalRead(BOUTON_PLAY);
   if((etatBoutonPlay != memoireBoutonPlay) && (etatBoutonPlay == HIGH)) {
     valBoutonPlay++;
     valBoutonPlay = valBoutonPlay % 2; // 2 valeurs possibles : 0, 1
